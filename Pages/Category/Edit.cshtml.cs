404a27751ce8bb440710030898b4f97777e66afd
huot <khouvtannhuot168@gmail.com>
2025-08-02 14:59:05 +0700

    - Add Navigation - Add Footer - Finish UI Category List, Create, Edit, Delete

---
+++ Pages/Category/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace blog_pt16.Pages.Category
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
